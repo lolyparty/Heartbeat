9b08a9a53fd5e3859b6d54cd95e9dab4b4c73b31
lolyparty <ololadeolayiwola01@gmail.com>
2020-08-04 04:36:21 +0100

    timer

--- app.js
+++ app.js
@@ -1,10 +1,75 @@
 var heart = document.querySelector('.heart');
 var beat = document.querySelector('#audiobeat');
+var playing = false;
+var tween = gsap.to('.heart',{
+    duration:1.05,
+    scale:1.4,
+    rotation:45,
+    ease: 'bounce',
+    repeat:-1,
+    paused:true,
+});
+
+// var timeInt = function(countSeconds){
+    
+// }
+
+// var countMinute = 1;
+//     var countSeconds = countMinute * 60;
+
+var time = function (){
+    const countMinute = 1;
+        let countSeconds = countMinute * 60;
+
+    // clearInterval(time)
+    setInterval(()=>{
+        var  minuteLeft = Math.floor(countSeconds/60);
+           if(minuteLeft < 10){
+               minuteLeft = '0' + minuteLeft;
+           }
+            var secondsLeft = countSeconds % 60;
+            if (secondsLeft < 10){
+                secondsLeft = '0' + secondsLeft;
+            }
+        
+            countSeconds--;
+            if(countSeconds < 0){
+                countSeconds = 0;
+            }
+        
+            var timer = minuteLeft + ':' + secondsLeft
+            // console.log(timer);
+        
+            document.querySelector('.span').textContent = timer;
+    }, 1000)
+    
+}
 
 heart.addEventListener('click', ()=>{
-    heart.style.animation = "heartbeat 0.55s ease-in-out infinite alternate";
-    beat.play();
+    
+
     beat.loop = true;
+    beat.onplaying = ()=>{
+        playing = true;
+        console.log('Playing');
+    }
+
+    beat.onpause = ()=>{
+        playing = false;
+        console.log('Paused');
+        time();
+    }
+
+    if(playing){
+        beat.pause();
+        tween.pause();
+        console.log('click to play');
+        document.querySelector('.timer').style.display = 'block';
+    }else{
+        beat.play();
+        tween.play();
+        console.log('click to pause');
+        document.querySelector('.timer').style.display = 'none';
+    }
 })
 
-//add greensock play pause continue countdown to disintegrate
